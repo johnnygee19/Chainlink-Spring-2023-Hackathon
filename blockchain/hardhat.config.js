ee955abfa5d6ff8a40c3d17e7acbe401f61ed98f
Johnny Gee <sigmamining19@gmail.com>
2023-05-21 12:18:07 -0700

    Update config file and install dotenv

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomicfoundation/hardhat-chai-matchers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.4.24",
+  networks: {
+    sepolia: {
+      url: process.env.INFURA_SEPOLIA_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
 };
